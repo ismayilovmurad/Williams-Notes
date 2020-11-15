9a590771cefcab102f4ed65ef969d6ee76b8b748
mdismayilov <muradismayilov6666@gmail.com>
2020-11-15 19:27:16 +0400

    Turn a data class into a Room entityq

--- WilliamsNotes/app/src/main/java/com/martiandeveloper/williamsnotes/model/Note.kt
+++ WilliamsNotes/app/src/main/java/com/martiandeveloper/williamsnotes/model/Note.kt
@@ -1,9 +1,17 @@
 package com.martiandeveloper.williamsnotes.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.martiandeveloper.williamsnotes.utils.NEW_NOTE_ID
 import java.util.*
 
-data class Note(var id: Int, var date: Date, var text: String) {
+@Entity(tableName = "notes")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var date: Date,
+    var text: String
+) {
     constructor() : this(NEW_NOTE_ID, Date(), "")
     constructor(date: Date, text: String) : this(NEW_NOTE_ID, date, text)
 }
