5be15c2ed437ec285a83f6c9c5dc70b3a20a75f4
mdismayilov <muradismayilov6666@gmail.com>
2020-11-16 09:06:27 +0400

    Implement database operations in a DAO

---
+++ WilliamsNotes/app/src/main/java/com/martiandeveloper/williamsnotes/database/NoteDao.kt
@@ -0,0 +1,23 @@
+package com.martiandeveloper.williamsnotes.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.martiandeveloper.williamsnotes.model.Note
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(note: Note)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertAll(notes: List<Note>)
+
+    @Query("SELECT * FROM notes ORDER BY date ASC")
+    fun getAll(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    fun getNoteById(id: Int): Note?
+}
